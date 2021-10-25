54747ad4ab704836c5a485ffd2280cfec3916a66
FlutLab.io <>
2021-10-25 11:10:28 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'iconcontent.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const activeCardColour = Color(0xFF1D1E33);
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -23,25 +27,45 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
                   children: [
                     Expanded(
-                      child: BoxPanel(colour: Color(0xFF1D1E33)),
+                      child: BoxPanel(
+                        colour: activeCardColour,
+                        cardChild: Column(
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.mars,
+                              size: 80.0,
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'Male',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Color(0xFF8D8E98),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                     Expanded(
-                      child: BoxPanel(colour: Color(0xFF1D1E33)),
+                      child: BoxPanel(colour: activeCardColour),
                     ),
                   ],
                 ),
               ),
               Expanded(
-                child: BoxPanel(colour: Color(0xFF1D1E33)),
+                child: BoxPanel(colour: activeCardColour),
               ),
               Expanded(
                 child: Row(
                   children: [
                     Expanded(
-                      child: BoxPanel(colour: Color(0xFF1D1E33)),
+                      child: BoxPanel(colour: activeCardColour),
                     ),
                     Expanded(
-                      child: BoxPanel(colour: Color(0xFF1D1E33)),
+                      child: BoxPanel(colour: activeCardColour),
                     ),
                   ],
                 ),
@@ -53,14 +77,16 @@ class _HomePageState extends State<HomePage> {
 }
 
 class BoxPanel extends StatelessWidget {
-  BoxPanel({@required this.colour});
+  BoxPanel({@required this.colour, this.cardChild});
 
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10.0),
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
         color: colour,
