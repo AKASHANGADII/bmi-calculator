27645a301f96da246c4821d2c8b4c6de7ea57f40
FlutLab.io <>
2021-10-25 11:36:46 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'iconcontent.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'genderBox.dart';
 
 const activeCardColour = Color(0xFF1D1E33);
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -14,97 +15,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text('BMI-Calculator'),
-          ),
+      appBar: AppBar(
+        title: Center(
+          child: Text('BMI-Calculator'),
         ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: BoxPanel(
-                        colour: activeCardColour,
-                        cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
-                      ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: BoxPanel(
+                      colour: activeCardColour,
+                      cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                     ),
-                    Expanded(
-                      child: BoxPanel(
-                        colour: activeCardColour,
-                        cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
-                      ),
+                  ),
+                  Expanded(
+                    child: BoxPanel(
+                      colour: activeCardColour,
+                      cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Expanded(
-                child: BoxPanel(colour: activeCardColour),
+            ),
+            Expanded(
+              flex: 2,
+              child: BoxPanel(colour: activeCardColour),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: BoxPanel(colour: activeCardColour),
+                  ),
+                  Expanded(
+                    child: BoxPanel(colour: activeCardColour),
+                  ),
+                ],
               ),
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: BoxPanel(colour: activeCardColour),
-                    ),
-                    Expanded(
-                      child: BoxPanel(colour: activeCardColour),
-                    ),
-                  ],
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Text(
+                  'CALCULATE',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFFFFFF),
+                  ),
                 ),
-              )
-            ],
-          ),
-        ));
-  }
-}
-
-class IconContent extends StatelessWidget {
-  IconContent({this.icon, this.label});
-
-  final IconData icon;
-  final String label;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          color: Color(0xFFFFFFFF),
-          size: 80.0,
+                color: Colors.red,
+              ),
+            )
+          ],
         ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class BoxPanel extends StatelessWidget {
-  BoxPanel({@required this.colour, this.cardChild});
-
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: colour,
       ),
     );
   }
