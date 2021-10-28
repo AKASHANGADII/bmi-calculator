b697dee3949ab847485c1abc0130467d1d2269f5
FlutLab.io <>
2021-10-28 14:44:36 +0000

    Autosave

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Gender selectedGender;
-  int height = 160;
+  int height = 170;
   int weight = 50;
   int age = 18;
   // Color maleCardColor = inactiveCardColour;
