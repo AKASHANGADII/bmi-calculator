1656935f5e9b7a732f21e2c94259b3d23f763ca5
FlutLab.io <>
2021-10-28 14:07:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
+      theme: ThemeData(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         textTheme: TextTheme(
