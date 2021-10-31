42d854934d4bd0d2f400a2d9ea2a1bc5eb84e130
Akash Angadi <81625153+AKASHANGADII@users.noreply.github.com>
2021-10-31 23:54:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.white),
-        ),
       ),
       home: HomePage(),
     );
   }
 }
+
