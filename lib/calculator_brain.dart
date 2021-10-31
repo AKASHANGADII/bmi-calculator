aeb0fe3f869ea345be79bc4f76ed4ee4e480a9c9
Akash Angadi <81625153+AKASHANGADII@users.noreply.github.com>
2021-10-31 23:56:14 +0530

    Create calculator_brain.dart

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
